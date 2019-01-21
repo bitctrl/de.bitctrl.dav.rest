7a7eed975001e19074a821beca66c00175e8dd8d
ChristianHoesel <christian.hoesel@bitctrl.de>
2019-01-21 11:01:18 +0100

    Test mit JerseyTest Classe

--- de.bitctrl.dav.rest.client/src/main/java/de/bitctrl/dav/rest/client/ClientExample.java
+++ de.bitctrl.dav.rest.client/src/main/java/de/bitctrl/dav/rest/client/ClientExample.java
@@ -1,8 +1,6 @@
 package de.bitctrl.dav.rest.client;
 
 import java.net.URI;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -21,16 +19,18 @@ import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 
 import de.bitctrl.dav.rest.api.SystemObjekt;
 import de.bitctrl.dav.rest.api.SystemObjektImpl;
 
-public class RestTest {
-	
+/**
+ * Beispiel Client.
+ * 
+ * @author BitCtrl Systems GmbH, ChHoesel
+ *
+ */
+public class ClientExample {
+
 	protected static Client client;
 
 	protected static final URI uri = UriBuilder.fromUri("http://localhost/").port(9998).build();
@@ -38,67 +38,61 @@ public class RestTest {
 	protected static WebTarget target;
 
 	protected static Map<String, NewCookie> cookies;
-	
+
 	private static class InsecureTrustManager implements X509TrustManager {
 
 		@Override
 		public void checkClientTrusted(java.security.cert.X509Certificate[] arg0, String arg1)
 				throws java.security.cert.CertificateException {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void checkServerTrusted(java.security.cert.X509Certificate[] arg0, String arg1)
 				throws java.security.cert.CertificateException {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-			  return new java.security.cert.X509Certificate[0];
+			return new java.security.cert.X509Certificate[0];
 		}
 	}
 
-
-	@BeforeClass
-	public static void beforeClass() throws NoSuchAlgorithmException, KeyManagementException {
+	public static void main(String[] args) throws Exception {
 		final ClientConfig config = new ClientConfig();
-		
+
 //		SSLContext ctx = SSLContext.getInstance("SSL");
 //		TrustManager[] trustAllCerts = { new InsecureTrustManager() };
 //		ctx.init(null, trustAllCerts, null);
-		
+
 		config.register(JacksonFeature.class);
-		
-		
+
 //		ClientBuilder.newBuilder().sslContext(ctx);
 		client = ClientBuilder.newClient(config);
 
 		target = client.target(uri);
-		
+
+		sendetSystemObjekt();
+
+		client.close();
+
 	}
-	
-	@Test
-	public void testSystemObjects() {
+
+	private static void sendetSystemObjekt() {
 		SystemObjekt obj = new SystemObjektImpl();
 		obj.setName("SystemObject1.");
 		obj.setId("bubub");
 		ArrayList<SystemObjekt> list = new ArrayList<>();
 		list.add(obj);
 		target.path("/systemobjekte").request().post(Entity.entity(list, MediaType.APPLICATION_JSON));
-		
+
 		Response response = target.path("/systemobjekte").request().get();
-		Collection<SystemObjekt> objecte = response.readEntity(new GenericType<List<SystemObjekt>>(){} );
-		
-		Assert.assertTrue(objecte.contains(obj));		
-	}
+		Collection<SystemObjekt> objecte = response.readEntity(new GenericType<List<SystemObjekt>>() {
+		});
 
-	@AfterClass
-	public static void afterClass() {
-		client.close();
 	}
-
 
 }
---
+++ de.bitctrl.dav.rest.server/src/test/java/RestTest.java
@@ -0,0 +1,46 @@
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.bitctrl.dav.rest.api.SystemObjekt;
+import de.bitctrl.dav.rest.api.SystemObjektImpl;
+import de.bitctrl.dav.rest.server.SystemobjekteImpl;
+
+public class RestTest extends JerseyTest  {
+	
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(SystemobjekteImpl.class);
+	}
+	
+	@Test
+	public void testSystemObjects() {
+		SystemObjekt obj = new SystemObjektImpl();
+		obj.setName("SystemObject1.");
+		obj.setId("bubub");
+		ArrayList<SystemObjekt> list = new ArrayList<>();
+		list.add(obj);
+		target("/systemobjekte").request().post(Entity.entity(list, MediaType.APPLICATION_JSON));
+		
+		Response response = target("/systemobjekte").request().get();
+		Collection<SystemObjekt> objecte = response.readEntity(new GenericType<List<SystemObjekt>>(){} );
+		
+		Assert.assertTrue(objecte.contains(obj));		
+	}
+
+
+
+}
