c4c94fb87937cfa702cefa806bdf2d337ca86fd3
Christian <ChristianHoesel@users.noreply.github.com>
2021-10-21 18:11:56 +0200

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branchPrefix: 'renovate/',
-  dryRun: true,
+  dryRun: false,
   username: 'renovate-release',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   onboarding: false,
